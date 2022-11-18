0820e15508ab9033438fea81cb89c02579378de7
Fernanda <sanjaya.dev20@gmail.com>
2022-11-18 16:58:47 +0700

    update file 2

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import { useState, useEffect } from "react";
 import logo from "../../logo.svg";
 import "../../App.css";
-import { GoogleLogin } from "react-google-login";
 
-const GOOGLECLIENTID = "979909028362-m77kbte3cel9jompo35lvn4fsg3vv882.apps.googleusercontent.com"
+
+// const GOOGLECLIENTID = "979909028362-m77kbte3cel9jompo35lvn4fsg3vv882.apps.googleusercontent.com"
 
 async function doLogin({ email, password }) {
   // Gunakan endpoint-mu sendiri
@@ -22,15 +22,15 @@ async function doLogin({ email, password }) {
   return data.token;
 }
 
-async function doLoginWithGoogle(token) {
-  const response = await fetch("http://localhost:8000/api/v1/auth/google", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ token }),
-  });
-  const data = await response.json();
-  return data.token;
-}
+// async function doLoginWithGoogle(token) {
+//   const response = await fetch("http://localhost:8000/api/v1/auth/google", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ token }),
+//   });
+//   const data = await response.json();
+//   return data.token;
+// }
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -52,23 +52,23 @@ function Login() {
       .finally(() => setIsLoading(false));
   }
 
-  const haldleSuccessGoogle = (response) => {
-    console.log(response);
-    console.log(response.tokenId);
-    if (response.tokenId) {
-      doLoginWithGoogle(response.tokenId)
-        .then((token) => {
-          localStorage.setItem("token", token);
-          setIsLoggedIn(token);
-        })
-        .catch((err) => console.log(err.message))
-        .finally(() => setIsLoading(false));
-    }
-  };
-  const haldleFailureGoogle = (response) => {
-    console.log(response);
-    alert(response);
-  };
+  // const haldleSuccessGoogle = (response) => {
+  //   console.log(response);
+  //   console.log(response.tokenId);
+  //   if (response.tokenId) {
+  //     doLoginWithGoogle(response.tokenId)
+  //       .then((token) => {
+  //         localStorage.setItem("token", token);
+  //         setIsLoggedIn(token);
+  //       })
+  //       .catch((err) => console.log(err.message))
+  //       .finally(() => setIsLoading(false));
+  //   }
+  // };
+  // const haldleFailureGoogle = (response) => {
+  //   console.log(response);
+  //   alert(response);
+  // };
 
   function handleLogout(e) {
     setIsLoading(true);
@@ -112,13 +112,13 @@ function Login() {
               value={password}
             />
             <input type="submit" value={isLoading ? "Loading" : "Login"} />
-            <GoogleLogin
+            {/* <GoogleLogin
               clientId={GOOGLECLIENTID}
               buttonText="Login with Google"
               onSuccess={haldleSuccessGoogle}
               onFailure={haldleFailureGoogle}
               cookiePolicy={"single_host_origin"}
-            />
+            /> */}
           </form>
         ) : (
           <input type="submit" value="Logout" onClick={handleLogout} />
